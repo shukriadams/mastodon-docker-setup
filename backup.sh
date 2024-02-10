f383399f4970bdd6fcc155e53b26cc6371bf0abb
Shukri Adams <shukri.adams@gmail.com>
2024-02-11 00:50:38 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,10 @@
+set -e
+
+NOW=$(date +%F)
+
+# backup postgres
+docker exec -it mastodon-db sh -c "pg_dumpall -U postgres > /tmp/all.sql"
+docker exec -it mastodon-db sh -c "cp /tmp/all.sql /postgres_backups/all.sql"
+
+# pack everything of value into zip
+zip -r ./backup_$NOW.zip ./redis ./postgres_backups ./public ./backup.sh ./docker-compose.yml ./.env.production ./proxy
