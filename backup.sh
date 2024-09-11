351bd3da3144705afc702da9333cae2aa9b663e4
Shukri Adams <shukri.adams@gmail.com>
2024-09-11 17:11:27 +0200

    added cache to zip ignore
    backing up cache will greatly bloat your mastodon backups

--- backup.sh
+++ backup.sh
@@ -7,4 +7,4 @@ docker exec -it mastodon-db sh -c "pg_dumpall -U postgres > /tmp/all.sql"
 docker exec -it mastodon-db sh -c "cp /tmp/all.sql /postgres_backups/all.sql"
 
 # pack everything of value into zip
-zip -r ./backup_$NOW.zip ./redis ./postgres_backups ./public ./backup.sh ./docker-compose.yml ./.env.production ./proxy
+zip -r ./backup_$NOW.zip ./redis ./postgres_backups ./public ./backup.sh ./docker-compose.yml ./.env.production ./proxy -x "public/system/cache/**"
